f7bf6e252b165a5dbf2679f8255387524ef455db
Olivia <olivia.wright101@gmail.com>
2020-01-08 22:25:02 +0000

    Completed isogram finder

--- hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
-
+  this.word = word
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
-}
+  const wordSplitUpAndUpperCased = this.word.toUpperCase().split("");
+  const duplicatesRemoved = [...new Set(wordSplitUpAndUpperCased)];
+  return wordSplitUpAndUpperCased.length === duplicatesRemoved.length
+};
 
 module.exports = IsogramFinder;
