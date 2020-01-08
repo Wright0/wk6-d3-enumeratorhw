8277e4a3d998a59501b97651317c484668529500
Olivia <olivia.wright101@gmail.com>
2020-01-08 19:21:04 +0000

    Finished MVP, starting extensions

--- models/traveller.js
+++ models/traveller.js
@@ -3,28 +3,32 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(journey => journey.startLocation)
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation)
 
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport)
 
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance > minDistance)
 };
 
-Traveller.prototype.calculateTotalDistanceTravelled = function () {
+// Traveller.prototype.calculateTotalDistanceTravelled = function () {
+//   return this.journeys.reduce((totalDistance, journey) => {
+//     return totalDistance + journey.distance
+//   })
+// };
 
-};
-
-Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//
+// };
 
 
 module.exports = Traveller;
