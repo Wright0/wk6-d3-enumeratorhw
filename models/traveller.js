7bc5175fa490ba6d830864d38041283934dea55b
Olivia <olivia.wright101@gmail.com>
2020-01-08 19:30:32 +0000

    Finished extension 1

--- models/traveller.js
+++ models/traveller.js
@@ -20,11 +20,11 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
   return this.journeys.filter(journey => journey.distance > minDistance)
 };
 
-// Traveller.prototype.calculateTotalDistanceTravelled = function () {
-//   return this.journeys.reduce((totalDistance, journey) => {
-//     return totalDistance + journey.distance
-//   })
-// };
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((totalDistance, journey) => {
+    return totalDistance + journey.distance
+  }, 0)
+};
 
 // Traveller.prototype.getUniqueModesOfTransport = function () {
 //
