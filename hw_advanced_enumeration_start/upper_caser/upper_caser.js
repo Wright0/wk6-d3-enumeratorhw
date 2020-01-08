1ab4ad39164d5eabaedb86197113e8a0f3e44634
Olivia <olivia.wright101@gmail.com>
2020-01-08 21:40:51 +0000

    Okay that worked the first time and now I'm conviced I did something wrong.

--- hw_advanced_enumeration_start/upper_caser/upper_caser.js
+++ hw_advanced_enumeration_start/upper_caser/upper_caser.js
@@ -1,8 +1,9 @@
 const UpperCaser = function (words) {
+  this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
-}
+  return this.words.map(string => string.toUpperCase());
+};
 
 module.exports = UpperCaser;
