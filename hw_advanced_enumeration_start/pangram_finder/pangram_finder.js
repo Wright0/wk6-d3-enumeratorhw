eef7a5eefee137d1d59f034f7e48aaa78ac88c98
Olivia <olivia.wright101@gmail.com>
2020-01-09 09:05:57 +0000

    Finished the pangram

--- hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
+++ hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,10 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
-
+  const phraseMadeLowerCase = this.phrase.toLowerCase();
+  return this.alphabet.every( letter => phraseMadeLowerCase.includes(letter) );
+};
 module.exports = PangramFinder;
